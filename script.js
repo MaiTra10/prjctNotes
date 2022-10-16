0748048b7c7413d268a825029d580dba31f43619
MaiTra10 <10maitra22@gmail.com>
2022-10-16 00:01:17 -0600

    feat
    Log in popup now has a 'sign up' and 'forgot password' subpage that has a sliding animation when you click 'Sign Up" or 'Forgot Password?'
    
    Known: need to change text size for the 'a' tag in 'forgot password' and 'sign up' page

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let loginBlock = document.getElementById("loginBlock");
 let bgBlur = document.getElementById("bgBlur");
+let formLI = document.getElementById("formLI");
+let formSU = document.getElementById("formSU");
+let formFP = document.getElementById("formFP");
 
 function openLoginBlock() {
 
@@ -12,5 +15,33 @@ function closeLoginBlock() {
 
     loginBlock.classList.remove("open-login-block");
     bgBlur.classList.remove("open-bg-blur");
+
+}
+
+function openSignUpBlock() {
+
+    formLI.classList.add("slide-l-i-right");
+    formSU.classList.add("slide-s-u-right");
+
+}
+
+function closeSignUpBlock() {
+
+    formLI.classList.remove("slide-l-i-right");
+    formSU.classList.remove("slide-s-u-right");
+
+}
+
+function openFrgPwdBlock() {
+
+    formLI.classList.add("slide-l-i-left");
+    formFP.classList.add("slide-f-p-left");
+
+}
+
+function closeFrgPwdBlock() {
+
+    formLI.classList.remove("slide-l-i-left");
+    formFP.classList.remove("slide-f-p-left");
 
 }
