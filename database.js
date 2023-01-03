84e3cded521c662e2adb38f6575a682dc8d3603f
MaiTra-P <55607989+MaiTra10@users.noreply.github.com>
2023-01-03 01:49:02 -0700

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,3 @@
+const url = 'https://ddwfnkclumtvrqohshfi.supabase.co';
 const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkd2Zua2NsdW10dnJxb2hzaGZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzI3MzMxNzksImV4cCI6MTk4ODMwOTE3OX0.S-wWD_b7_JVV2RTf7FWok6W_THWUXDvJtB6dUzFNqlE';
+var supabase = supabase.createClient(url, key);
