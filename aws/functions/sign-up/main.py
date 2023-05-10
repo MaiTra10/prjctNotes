17e183a430326da3d450d359433e85849633ad51
MaiTra-P <55607989+MaiTra10@users.noreply.github.com>
2023-05-10 03:22:38 -0600

    feat
    Sign up is now functional. Adds the email with its respective password into the DynamoDB and has a system for disallowing repeated emails. On successful account creation, you get sent back to the login page and it clears the sign up form.

--- aws/functions/sign-up/main.py
+++ aws/functions/sign-up/main.py
@@ -1,5 +1,36 @@
 import boto3
+import json
+
+dynamoDB = boto3.resource("dynamodb")
+table_name = "prjctNotes-DB"
+table = dynamoDB.Table(table_name)
 
 def lambda_sign_up(event, ctx):
 
-    print("test")
+    url_request_type = event["requestContext"]["http"]["method"]
+    
+    if url_request_type == "POST":
+        
+        data = json.loads(event["body"])
+        
+        email = data["email"]
+        
+        password = data["password"]
+
+        resp = table.get_item(TableName=table_name, Key={"email": email, "ctx": "password"})
+
+        if "Item" in resp:
+            
+            return {"Status Code": 409}
+            
+        else:
+        
+            return table.put_item(TableName=table_name, Item={"email": email, "ctx": "password", "password": password})
+        
+    else:
+        
+        return {
+            "Error": "Incorrect URL Request Type",
+            "Want": "POST",
+            "Got": url_request_type
+        }
--- script_aws.js
+++ script_aws.js
@@ -1,7 +1,65 @@
-function sign_in_btn(e) {
+async function sign_in_btn(e) {
 
     const form = e.parentElement.parentElement.elements;
     const email = form.email.value;
     const pwd = form.pwd.value;
+
+
+}
+
+async function sign_up_btn(e) {
+
+    const form = e.parentElement.elements;
+    const email = form.email.value;
+    const pwd = form.pwd.value;
+    const data = {
+        email: email,
+        password: pwd
+    }
+    fetch("https://l5pcnftq5xix72zfg6ssutgsom0akjyy.lambda-url.us-west-2.on.aws/", {
+
+        method : "POST",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        },
+        body : JSON.stringify(data)
+
+    }).then(
+        response => response.json()
+    ).then(
+        function(html) {
+
+            if(html["Status Code"] == 409){
+
+                console.log("Account Already Exists!")
+                //Add proper fail and success messages
+
+            } else {
+
+                console.log("Account Created!")
+            
+                formLI.classList.remove("slide-right");
+                formSU.classList.remove("slide-right");
+            
+                formLI.classList.remove("slide-left");
+                formFP.classList.remove("slide-left");
+            
+                loginBlock.classList.remove("s-u-resize");
+                loginBlock.classList.remove("f-p-resize");
+
+                formSU.reset();
+
+            }
+
+        }
+    );
+
+}
+
+async function forgot_pass_btn(e) {
+
+    const form = e.parentElement.parentElement.elements;
+    const email = form.email.value;
+
 
 }
